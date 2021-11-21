cef02b11443bde8138118b541598f1cafb2c46cd
bryanexl2026 <bryanexl2026@yahoo.com>
2021-11-21 12:46:52 -0500

    Update prints.py

--- Hags-Hamlets-main_bryan_update/hagsnhamlets/prints.py
+++ Hags-Hamlets-main_bryan_update/hagsnhamlets/prints.py
@@ -3,18 +3,29 @@
 #from playsound import playsound
 
 import time
+#from ascii_art import *
+
+i = 0 
 
 #prints with delay and sound
 def prints(var, delay =.1, sound= True):
+    global i 
+    border_left = ["#|.  ||     ", "#| . ||     ", "#|.  ||     ", "#| . ||     "]
+    border_right = ["        ||  .|#", "        || . |#", "        ||  .|#", "        || . |#"] 
 
+    
     if(var == None):
         time.sleep(0)
 
     else:
+
+        i += 1
+        if i > 3: 
+            i = 0 
         time.sleep(delay)
         #if sound:
             #playsound("line.wav", False)
-        print(var)
+        print(border_left[i] + var)
 
 #returnns with delay and sound    
 def printr(var, delay =.1):
@@ -25,3 +36,4 @@ def printr(var, delay =.1):
         time.sleep(delay)
         #playsound("line.wav", False)
         return var
+

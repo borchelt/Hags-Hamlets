20904d5073cf5f7a65fb2aa0eb11a854dcaee7ef
bryanexl2026 <bryanexl2026@yahoo.com>
2021-11-28 13:23:24 -0500

    item list
    item list from the google doc

--- hagsnhamlets/item_list.py
+++ hagsnhamlets/item_list.py
@@ -11,23 +11,50 @@ item("Ruby","A large ruby, found in the Hangman's Tree. You suspect it has somet
 
 item("Silver Ore", "A large cache of raw Silver Ore. The blacksmith said that they would be able to fashion you a much stronger breastplate if they had this.", pickup=True)
 
+#POTIONS
 
-#GENERAL OBJECTS
+item("Old Bones", "A potion ingredient", pickup=True)
+item("Dwarf's Hair", "A potion ingredient", pickup=True)
+item("Nasty Herb", "A potion ingredient.", pickup=True)
+item("tunnel Sludge", "A potion ingredient", pickup=True)
+item("Somebody's fingernail", "A potion ingredient", pickup=True)
+item("Old Bark", "A potion ingredient.", pickup=True)
+item("Empty Flask", "Stores Potions", pickup=True)
+item("Heart Potion", "Heals damage on use.", pickup=True)
+item("Potion of Despising", "Buffs your damage for a short period of time.", pickup=True)
+item("Dumb Luck Elixir", "Helps you win the speakeasy games more easily.", pickup=True)
+item("Wondrous Brew", "This potion will randomly buff one stat permanently.", pickup=True)
+
+
+
+#Trinkets
+
+item("Old Doll", "Found in the cottage. While you hold this item, Shadows in the Deep Woods will grant you XP.", pickup=True)
+item("Hangman's Rope", "Found at the Hangman's Tree. While held, you deal bonus damage to undead creatures.", pickup=True)
+item("Rat's Pack", "Dropped by the Rat King. This pack grants the player additional inventory space.", pickup=True)
+item("Hag's Eye", "Dropped by any defeated Hag. Staring into it may allow you to glimpse the future. Be warned!", pickup=True)
+item("Unsure Tombstone", "This tombstone...It lists all of your previous lives. Would you like to read it?", pickup=False)
+item("The Soul", "You are finally able to rest.", pickup=True)
+item("The Last Kernel", "Traded to you by the farmer. This item grants the holder bonus health.", pickup=True)
+item("Dark Cloak", "Traded to you by the Thief King. The wearer is granted the ability to avoid unwanted encounters.", pickup=True)
+item("The Book of Spells", "Traded to you by the librarian. Casting spells from this book results in a random spell being cast.", pickup=True)
+
+#MISC
+
+item("Broken Painting", "Found in the barn. Trade this to the farmer for something.", pickup=True)
+item("Gold tooth", "Found in the speakeasy. Trade to the merchant for one free item", pickup=True)
+item("Broken Dagger", "Found in the tunnels. There's a mark on here you recognize...The mark of the Thief King.", pickup=True)
+item("The First Book", "Found in the deep woods. Trade this to the librarian for something.", pickup=True)
+item("A Broken Watch", "Tells you when to leave and when to come back to the game. Doing so gives you a bonus to luck.", pickup=True)
 item("Bottle","A glass bottle.", pickup=True)
-
 item("Glass", "A simple wine glass.", pickup=True), 
-
 item("Silver  Fork", "An eating utensil.", pickup=True)
-
 item("Fine Plate", "A decorative, elegant plate.", pickup=True)
-
 item("Barrel", "A large wooden barrel.", pickup=False)
-
 item("Bed", "A comfy looking bed.", pickup=False)
-
 item("Book", "A dusty book.", pickup=True)
-
 item("Pamphlet", "A tattered pamphlet that has somehow withstood the test of time.", pickup=True)
+
 
 
 

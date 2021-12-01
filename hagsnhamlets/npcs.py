ad932db4a41896089ccdb09e7edab017127e449e
bryanexl2026 <bryanexl2026@yahoo.com>
2021-11-30 22:54:42 -0500

    finished dialogue
    need to add more shop inventories but here's dialogue at least for all the chars. start of quest line connections.

--- hagsnhamlets/npcs.py
+++ hagsnhamlets/npcs.py
@@ -120,8 +120,227 @@ def innkeeper_dialogue():
 
     dialogue(dialogue_options, dialogue_outcome)
 
+def blacksmith_dialogue():
+
+    blacksmith_ascii()
+    prints("")
+
+    dialogue_options = [
+    "     *** TRADE *** What do you have for sale?", 
+    "     What is your name?", 
+    "     What can you tell me about the Hamlet?",
+    "     *** QUEST *** Looking for work."
+    "     Goodbye."
+    ]
+
+    dialogue_outcome = [
+        "   \"I have plenty of wares for sale. That is, of course, supposing you've got the coin.\" ", 
+        "   \"Sebastien Steelscraper's the name. Pleasure to meet you...er...person.\" ",
+        "   \"Well the whole place has gone to shite since the Hags came and lay their curse upon us. Before that we were just a bunch of quiet folk.\" ",
+        "   \"The only kind of work I've got right now is less than pleasant. Would you like to hear about it? (Y/N) \" ", 
+
+        "   \"See you later! \" "
+            ]
+        
+        #if player has quest item
+
+        #option
+        #"  I have something for you, Sebastien.",
+        #outcome 
+        #"  My oh my! The silver ore. You must have gone through a lot of trouble to retrieve this. I'll get right to work making the breastplate!"
+        
+    dialogue(dialogue_options, dialogue_outcome)
+
+def librarian_dialogue():
+    dialogue_options = [
+    "     Who are you? ", 
+    "     What are doing right now?", 
+    "     Where are we?",
+    "     What can you tell me about the Hags' Curse?"
+    "     Goodbye."
+    ]
+
+    dialogue_outcome = [
+        "   \"Regina Quillspeth, at your service. Are you here because you're actually interested in books?\" ", 
+        "   \"I was studying old magical texts until you came along. Not that I mind the company. No one ever comes here.\" Regina sighs and sits back in her chair a little bit, pouting. ",
+        "   \"Where does it look like you are? This is the hamlet library. The monks that once ran this place left behind so much knowledge.\" ",
+        "   \"Well, I know that the monks all ran away or were murdered by the Hags. Now it's just me, left to look after all of their years of hard work. Uh...they're evil?\" ",
+        "   \"Come back and visit me some time!"
+
+        #gets quest to learn about potion
+        #talks to librarian and gets another option
+
+        #"  INSERT NAME told me that you might know where to find a potion recipe? Could you help me?"
+        
+        #outcome
+        #"  \"YES!!!!,\" Regina cheers, jumping from her chair, "I've been waiting for someone to ask me for help like this!\" "
+
+            ]
+        
+    dialogue(dialogue_options, dialogue_outcome)
+
+def merchant_dialogue():
+
+    dialogue_options = [
+    "     *** TRADE ***  What are you selling, friend? ", 
+    "     What is your name?", 
+    "     Where are you from?",
+    "     What do you know about the Hags' Curse?",
+    "     Goodbye."
+    ]
+
+    dialogue_outcome = [
+        "   \"Many curious things, for the right price.\" ", 
+        "   \"My name is Azni.\" ",
+        "   \"A place far beyond the borders of the woods.\" ",
+        "   \"Curse? What curse?\" Azni looks you up and down, gauging your reaction. He bursts into laughter, \"Hah! They're terrible, but I still must laugh.\" ",
+        "   \"Come back anytime, friend. \" "
+            ]
+        
+    dialogue(dialogue_options, dialogue_outcome)
+
+def general_store_owner_dialogue():
+
+    dialogue_options = [
+    "     *** TRADE *** I'd like to see your wares.", 
+    "     What is your name?", 
+    "     How is business?",
+    "     *** QUEST *** Is there anything that you need help with?",
+    "     Goodbye."
+    ]
+
+    dialogue_outcome = [
+        "   \"Of course! I've the best crafting materials in the land.\" ", 
+        "   \"Mr. Barry Barrelpine, humbly at your service. Anything you need, anything at all, I will sell it to you!\" ",
+        "   \"Fine. Why do you ask? Who have you been talking to?\" ",
+        "   \"Okay. Listen...I shouldn't be talking about this, but I owe a lot of money to Hank in the Secret Speakeasy in the Sewers. Could you see about settling my debt for me?\""
+        "   \"See you later! \" "
+            ]
+        
+    dialogue(dialogue_options, dialogue_outcome)
+
+def farmer_dialogue():
+    dialogue_options = [
+    "     Wait! Stop running! What has happened to you?", 
+    "     What is your name?", 
+    "     Where is your house?",
+    "     What can I do?",
+    "     Goodbye."
+    ]
+
+    dialogue_outcome = [
+        "   \"AHHHH!!! My house! Our house! She came...d-d-destroyed it all! What am I going to do?\" ", 
+        "   \"P-P-P-Podrick Pebbleton, s-s-sir. \" ",
+        "   \"Back...th-through the fi-field...Over the brook, down the trail past the old Oak for a bit.\" ",
+        "   \"Why? Are you some kind of hero? I-I...I...\" Podrick breaks into tears, \"I just want my house back!\" ",
+        "   \"I'll be alone in this world forever, it seems.\" "
+            ]
+        
+    dialogue(dialogue_options, dialogue_outcome)
+
+def thief_king_dialogue():
+    
+    dialogue_options = [
+    "     Are you the one they call the Thief King?", 
+    "     Why do you live in the Sewers?", 
+    "     Are you going to let me live?",
+    "     Are you working for the Hags?",
+    "     *** QUEST *** Any jobs you that need doing?",
+    "     Goodbye."
+    ]
+
+    dialogue_outcome = [
+        "   \"...Is the name not obvious enough?\" ", 
+        "   \"I like the way it smells,\" he says with scowl, \"The people of the hamlet don't take kindly to thieves walking about.\" ",
+        "   \"Give me a good reason to and I might.\" ",
+        "   \"Don't be ridiculous. They can sod right off.\" "
+        "   \"There's always work for willing hands. Are you sure you're up to it?",
+        "   \"I'll be here.\"."
+            ]
+        
+    dialogue(dialogue_options, dialogue_outcome)
+
+def hunter_dialogue():
+    
+    dialogue_options = [
+    "     Who are you?", 
+    "     What are you doing out here?", 
+    "     What do you know about the Hags' Curse?",
+    "     *** QUEST *** You look troubled. Can I help you in some way?"
+    "     Goodbye."
+    ]
+
+    dialogue_outcome = [
+        "   \"The name is Arthur. Arthur Edenbough. \" ", 
+        "   \"I thought I was going to finally fell that buck. Now you're here.\" ",
+        "   \"I've never seen one of them personally, but these woods are teeming with monsters. I don't past the brambles. It's too dark.\" ",
+        "   \"I'm so terribly thirsty. If you could bring me a cold beer, I'd be more than happy to share my map with you.\" ", 
+        "   \"Happy hunting!\" "
+            ]
+        
+    dialogue(dialogue_options, dialogue_outcome)
+
+def drunkard_dialogue():
+
+    dialogue_options = [
+    "     Try to wake the drunkard."
+    "     Pour his beer on him."
+    "     Knock on the table."
+    "     Clap loudly."
+    "     Leave."
+    ]
+
+    dialogue_outcome = [
+        "   He keeps snoring. There's no difference. ", 
+        "   The drunkard is wet now, but they're still out like a light. ",
+        "   You've knocked on the table. The drunkard flatulates loudly, wipes his nose, and keeps sleeping."
+        "   The drunkard, in his sleep, claps back."
+        "   The drunkard wakes up briefly, and looks around with squinted vision. Seconds later, he falls asleep again. "
+            ]
+        
+    dialogue(dialogue_options, dialogue_outcome)
+
+def dwarven_miner_dialogue():
+
+    dialogue_options = [
+    "     What is your name?", 
+    "     Are you in danger?", 
+    "     What can you tell me about these mines?",
+    "     *** QUEST *** Would you like my help?"
+    "     Goodbye."
+    ]
+
+    dialogue_outcome = [
+        "   \"Ztolo Zan I my name, stranger. Pleased to make your acquaintance. \" ", 
+        "   \"Not particularly. I was, not long ago. Damned cave in....\" ",
+        "   \"These mines are some of the oldest in all the land. The tunnels reach all the way to Urzal-ka!\" ",
+        "   \"Are you serious? Well, there's been a giant spider causing cave ins lately. Slows up production. Want to make a little money?"
+        "   \"See you later! \" "
+            ]
+        
+    dialogue(dialogue_options, dialogue_outcome)
+
+def guard_dialogue():
+
+    dialogue_options = [
+    "     I'm friendly! I swear!", 
+    "     What are you doing out here?", 
+    "     What can you tell me about where we are?",
+    "     Goodbye."
+    ]
+
+    dialogue_outcome = [
+        "   \"How should we know? Keep your hands off those weapons.\" ", 
+        "   \"We are the last of the guard. All of the others have been killed off.\" ",
+        "   \"This is our last defense from the outside world. Having the Hags in here with us is hard enough!\" ",
+        "   \"See you later! \" "
+            ]
+        
+    dialogue(dialogue_options, dialogue_outcome)
+    
 global tradable_npcs
-tradable_npcs = ["innkeeper", "blacksmith"]
+tradable_npcs = ["innkeeper", "blacksmith", "librarian", "merchant", "general store owner", "farmer", "thief king", "hunter", "dwarven miner"]
+    #if target npc is non-merchant type and player does not have correct quest item, trade option should not appear 
 
 def trade(target_npc):
 
